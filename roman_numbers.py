75175bd0b15ff02893e4f2a9141f94438d3bf157
Michał Prądzyński <pradzynski.michal@gmail.com>
2018-11-18 18:07:43 +0100

    roman_numbers - converter and get_roman_number functions implemented

--- roman_numbers.py
+++ roman_numbers.py
@@ -1,5 +1,6 @@
 # This program converts an arabic number into a roman number.
 
+
 def number_input():
     while True:
         try:
@@ -13,5 +14,48 @@ def number_input():
             return arabic_number
 
 
+def converter(list_name, index, var_1, var_2, var_3):
+    if list_name[index] == "9":
+            return (var_3 + var_1)
+    elif list_name[index] >= "6":
+        return (var_2 + var_3 * (((int(list_name[index])-5))))
+    elif list_name[index] == "5":
+        return (var_2)
+    elif list_name[index] == "4":
+        return (var_3 + var_2)
+    elif list_name[index] >= "1":
+        return (var_3 * (int(list_name[index])))
+
+
+def get_roman_number(arabic_number):
+
+    place_values = list(str(arabic_number))
+    roman_number = []
+
+    if len(place_values) == 4:
+        roman_number.append("M" * int(place_values[0]))
+        roman_number.append(converter(place_values, 1, "M", "D", "C"))
+        roman_number.append(converter(place_values, 2, "C", "L", "X"))
+        roman_number.append(converter(place_values, 3, "X", "V", "I"))
+
+    elif len(place_values) == 3:
+        roman_number.append(converter(place_values, 0, "M", "D", "C"))
+        roman_number.append(converter(place_values, 1, "C", "L", "X"))
+        roman_number.append(converter(place_values, 2, "X", "V", "I"))
+
+    elif len(place_values) == 2:
+        roman_number.append(converter(place_values, 0, "C", "L", "X"))
+        roman_number.append(converter(place_values, 1, "X", "V", "I"))
+
+    elif len(place_values) == 1:
+        if place_values[0] == "0":
+            roman_number.append("nulla")
+        else:
+            roman_number.append(converter(place_values, 0, "X", "V", "I"))
+
+    return "".join(roman_number)
+
+
 if __name__ == "__main__":
-    number_input()
+    arabic_number = number_input()
+    print("The roman number: {}".format(get_roman_number(arabic_number)))
