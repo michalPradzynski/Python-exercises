b3a38a190615e8a95bef8e2bfa1f99b0535abedd
Michał Prądzyński <pradzynski.michal@gmail.com>
2018-12-27 15:53:17 +0100

    added calculating functions

--- one_line_calculator.py
+++ one_line_calculator.py
@@ -1,5 +1,32 @@
 # This program can do simple calculations from a single line input.
 
+def add(x,y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    return x / y
+
+def power(x, y):
+    return x ** y
+
+def calculate(x, y, symbol):
+    if symbol = "+":
+        add(x, y)
+    elif symbol = "-":
+        subtract(x, y)
+    elif symbol = "*":
+        multiply(x, y)
+    elif symbol = "/":
+        divide(x, y)
+    elif symbol = "^":
+        power(x, y)
+
 def abc():
     input_equation = input("Enter your equation: ")
     equation = "".join(input_equation)
@@ -21,6 +48,11 @@ def abc():
     print(number1)
     print(sign)
     print(number2)
+
+    number1 = "".join(number1)
+    number2 = "".join(number2)
+
+
 
 
 
