f5d8dcf1df0ee98d2865ac05e76edde3835ef7b5
Michał Prądzyński <pradzynski.michal@gmail.com>
2018-12-27 16:07:37 +0100

    simpliest working version

--- one_line_calculator.py
+++ one_line_calculator.py
@@ -1,31 +1,38 @@
 # This program can do simple calculations from a single line input.
 
-def add(x,y):
+
+def add(x, y):
     return x + y
+
 
 def subtract(x, y):
     return x - y
 
+
 def multiply(x, y):
     return x * y
+
 
 def divide(x, y):
     return x / y
 
+
 def power(x, y):
     return x ** y
 
+
 def calculate(x, y, symbol):
-    if symbol = "+":
-        add(x, y)
-    elif symbol = "-":
-        subtract(x, y)
-    elif symbol = "*":
-        multiply(x, y)
-    elif symbol = "/":
-        divide(x, y)
-    elif symbol = "^":
-        power(x, y)
+    if symbol == "+":
+        return add(x, y)
+    elif symbol == "-":
+        return subtract(x, y)
+    elif symbol == "*":
+        return multiply(x, y)
+    elif symbol == "/":
+        return divide(x, y)
+    elif symbol == "^":
+        return power(x, y)
+
 
 def abc():
     input_equation = input("Enter your equation: ")
@@ -36,29 +43,26 @@ def abc():
     sign = None
 
     for character in equation:
-        if sign == None:
+        if sign is None:
             if character.isdigit():
-                number1.append(int(character))
+                number1.append(character)
             elif character in symbols:
                 sign = character
         else:
             if character.isdigit():
-                number2.append(int(character))
+                number2.append(character)
 
     print(number1)
     print(sign)
     print(number2)
 
-    number1 = "".join(number1)
-    number2 = "".join(number2)
+    num1 = int("".join(number1))
+    num2 = int("".join(number2))
 
+    result = calculate(num1, num2, sign)
 
-
-
-
-
-
+    return result
 
 
 if __name__ == "__main__":
-    abc()
+    print(abc())
